30a97852ff5f20ebe6449802de356cff6be3a242
Juan Pablo <117771337+JuanPabloDV@users.noreply.github.com>
2024-01-06 15:44:43 -0300

    Atualiza "sobre mim"

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -21,9 +21,9 @@ const About = () => {
         <div className="about-wrapper">
           <div className="about-left">
             <h2 className="title">Sobre mim</h2>
-            <p>Juan Pablo, 19 anos, cursando o 4º semestre de <strong>Sistemas da Informação</strong>. Possuo habilidades em desenvolvimento web front-end, experiência com <strong>Bootstrap</strong> para criar interfaces responsivas e <strong>Vue.js</strong> para a construção de aplicações dinâmicas. Além disso, estou em busca de aprimorar minhas habilidades no back-end, assim podendo futuramente me considerar um desenvolvedor Full-stack.</p>
+            <p>Juan Pablo, 19 anos, cursando o 5º semestre de <strong>Sistemas da Informação</strong>. Possuo habilidades em desenvolvimento web front-end, experiência com <strong>Bootstrap</strong> para criar interfaces responsivas e <strong>Vue.js</strong> para a construção de aplicações dinâmicas. Além disso, estou em busca de aprimorar minhas habilidades no back-end com <strong>Node</strong> e <strong>Java</strong> , assim podendo futuramente me considerar um desenvolvedor Full-stack.</p>
             <div>
-              <button><a href="https://drive.google.com/file/d/1guiU9oCYUJfgnQ9QwcNQAsrSCd7KZygI/view?usp=drive_link" target='_blank' rel='noopener noreferrer'>Currículo</a></button>
+              <button><a href="https://drive.google.com/file/d/1UNR2-hpPu4GvLNi1O_atK2Q7P_FMAGwt/view?usp=sharing" target='_blank' rel='noopener noreferrer'>Currículo</a></button>
             </div>
           </div>
   
